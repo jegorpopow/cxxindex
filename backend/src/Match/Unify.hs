342c7e416009bed27840b7218e92caf5397a52f7
Jegor Popow <juicedogegor@gmail.com>
2025-11-13 16:39:54 +0300

    withdraw unification algorithm

---
+++ backend/src/Match/Unify.hs
@@ -0,0 +1,23 @@
+module Match.Unify where
+
+import Data.List (permutations)
+import Data.Maybe (mapMaybe)
+import Model.CType
+import Model.Query
+
+unify :: CQuery -> CDecl -> Maybe CMatch
+unify CQuery {target = CDeclType {template_args, arguments, result}} CDecl {name, ctype, location} =
+  getFirst (matchOne ctype . (\args_permutation -> CDeclType template_args args_permutation result) <$> permutations arguments)
+  where
+    matchOne :: CDeclType -> CDeclType -> Maybe CMatch
+    matchOne CDeclType {template_args, arguments, result} CDeclType {template_args', arguments', result'} = udefined
+    matchOne _ _ = Nothing
+    unifyArgs :: CType -> CType -> Maybe CMatch
+    unifyArgs = undefined
+    getFirst :: [Maybe a] -> Maybe a
+    getFirst [] = Nothing
+    getFirst (h@(Just _) : _) = h
+    getFirst (Nothing : rest) = getFirst rest
+
+search :: CQuery -> CIndex -> [CMatch]
+search query = Data.Maybe.mapMaybe (unify query)
--- backend/src/Model/Query.hs
+++ backend/src/Model/Query.hs
@@ -2,8 +2,6 @@ module Model.Query where
 
 import Model.CType
 import Data.Map
-import Data.Maybe(mapMaybe)
-
 
 -- data Query = Query {context :: [(CVar, CKind)], arguments :: Set [CType], result :: CType}
 newtype CQuery = CQuery {target :: CDeclType} deriving (Show)
@@ -11,9 +9,3 @@ data CMatch = CMatch {queried :: CQuery, matched :: CDecl, unification :: Map CV
 
 prettyPrint :: CMatch -> String
 prettyPrint = undefined
-
-unify :: CQuery -> CDecl -> Maybe CMatch
-unify = undefined
-
-search :: CQuery -> CIndex -> [CMatch]
-search query  = Data.Maybe.mapMaybe (unify query) 
